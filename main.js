8bc0f0299fe3108a345997540fe29ad0970e4aa6
albertos2001 <65310323+albertos2001@users.noreply.github.com>
2020-11-25 04:55:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,3 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
-
